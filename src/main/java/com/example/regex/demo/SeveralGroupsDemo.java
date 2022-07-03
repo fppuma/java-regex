fa36e5e5c5f5ed843eafc023a9cb999108ab281d
Francisco Puma <fppuma@gmail.com>
2022-07-03 18:18:25 -0500

    add new matcher

--- src/main/java/com/example/regex/demo/SeveralGroupsDemo.java
+++ src/main/java/com/example/regex/demo/SeveralGroupsDemo.java
@@ -19,10 +19,15 @@ public class SeveralGroupsDemo {
     Pattern p = Pattern.compile("(\\d)([a-z])");
 
     // matcher(input string)
-    Matcher m = p.matcher("3a + 5b = 25, where a and b are integers");
-
-    List<String> items = GroupUtils.getItems(m);
+    String input = "3a + 5b = 25, where a and b are integers";
+    
+    Matcher m1 = p.matcher(input);
+    List<String> items = GroupUtils.getItems(m1);
     logger.info("items: {}", items);// items: [3a, 3, a, 5b, 5, b]
+    
+    Matcher m2 = p.matcher(input);
+    List<String> items2 = GroupUtils.getItemsFromUniqueGroup(m2);
+    logger.info("items2: {}", items2);//items2: [3a, 5b]
 
   }
 
