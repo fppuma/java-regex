8d908037f9e779815634a40e506f6e5f2fe647e3
Francisco Puma <fppuma@gmail.com>
2022-07-03 21:46:51 -0500

    unit test

--- src/test/java/com/example/regex/util/ManyGroupsTest.java
+++ src/test/java/com/example/regex/util/ManyGroupsTest.java
@@ -2,6 +2,7 @@ package com.example.regex.util;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.util.List;
 import java.util.regex.Matcher;
@@ -39,6 +40,111 @@ public class ManyGroupsTest {
       () -> assertEquals("https://github.com", items.get(0)),
       () -> assertEquals("https://", items.get(1)), //protocol
       () -> assertEquals("github.com", items.get(2))//domain
+    );
+  }
+  
+  @Test
+  @DisplayName("Protocol and Domain")
+  public void testProtocolAndDomain02() {
+    
+    String regex = "(https?://)([^:^/]*)";
+    Pattern p = Pattern.compile(regex);
+
+    String github = "http://localhost:8080/marketplace";
+    Matcher m = p.matcher(github);
+    List<String> items = GroupUtils.getItemsByGroups(m);
+    logger.info("items: {}", items);
+    assertAll("localhost", 
+      () -> assertEquals(3, items.size()),
+      () -> assertEquals("http://localhost", items.get(0)),
+      () -> assertEquals("http://", items.get(1)), //protocol
+      () -> assertEquals("localhost", items.get(2))//domain
+    );
+  }
+  
+  @Test
+  @DisplayName("Protocol, Domain and Port")
+  public void testProtocolDomainAndPort() {
+
+    //(:\\d*)? :digits (0 or 1)
+    String regex = "(https?://)([^:^/]*)(:\\d*)?";
+    Pattern p = Pattern.compile(regex);
+
+    String localhost = "http://localhost:8080/marketplace";
+    Matcher m = p.matcher(localhost);
+    List<String> items = GroupUtils.getItemsByGroups(m);
+    logger.info("items: {}", items);
+    assertAll("localhost", 
+      () -> assertEquals(4, items.size()),
+      () -> assertEquals("http://localhost:8080", items.get(0)),
+      () -> assertEquals("http://", items.get(1)),  //protocol
+      () -> assertEquals("localhost", items.get(2)),//domain
+      () -> assertEquals(":8080", items.get(3))      //port
+    );
+  }
+  
+  @Test
+  @DisplayName("Protocol, Domain and Port")
+  public void testProtocolDomainAndPort02() {
+
+    //(:\\d*)? :digits (0 or 1)
+    String regex = "(https?://)([^:^/]*)(:\\d*)?";
+    Pattern p = Pattern.compile(regex);
+
+    String localhost = "https://github.com/marketplace";
+    Matcher m = p.matcher(localhost);
+    List<String> items = GroupUtils.getItemsByGroups(m);
+    logger.info("items: {}", items);
+    assertAll("localhost", 
+      () -> assertEquals(4, items.size()),
+      () -> assertEquals("https://github.com", items.get(0)),
+      () -> assertEquals("https://", items.get(1)),  //protocol
+      () -> assertEquals("github.com", items.get(2)),//domain
+      () -> assertNull(items.get(3))                 //without port
+    );
+  }
+  
+  @Test
+  @DisplayName("Protocol, Domain, Port and Uri")
+  public void testProtocolDomainPortAndUri() {
+
+    //(.*)? any characters
+    String regex = "(https?://)([^:^/]*)(:\\d*)?(.*)?";
+    Pattern p = Pattern.compile(regex);
+
+    String localhost = "https://github.com/marketplace";
+    Matcher m = p.matcher(localhost);
+    List<String> items = GroupUtils.getItemsByGroups(m);
+    logger.info("items: {}", items);
+    assertAll("localhost", 
+      () -> assertEquals(5, items.size()),
+      () -> assertEquals("https://github.com/marketplace", items.get(0)),
+      () -> assertEquals("https://", items.get(1)),    //protocol
+      () -> assertEquals("github.com", items.get(2)),  //domain
+      () -> assertNull(items.get(3)),                  //without port
+      () -> assertEquals("/marketplace", items.get(4))//uri
+    );
+  }
+  
+  @Test
+  @DisplayName("Localhost: Protocol, Domain, Port and Uri")
+  public void testProtocolDomainPortAndUri02() {
+
+    //(.*)? any characters
+    String regex = "(https?://)([^:^/]*)(:\\d*)?(.*)?";
+    Pattern p = Pattern.compile(regex);
+
+    String localhost = "http://localhost:8080/marketplace";
+    Matcher m = p.matcher(localhost);
+    List<String> items = GroupUtils.getItemsByGroups(m);
+    logger.info("items: {}", items);
+    assertAll("localhost", 
+      () -> assertEquals(5, items.size()),
+      () -> assertEquals("http://localhost:8080/marketplace", items.get(0)),
+      () -> assertEquals("http://", items.get(1)),    //protocol
+      () -> assertEquals("localhost", items.get(2)),  //domain
+      () -> assertEquals(":8080", items.get(3)),      //port
+      () -> assertEquals("/marketplace", items.get(4))//uri
     );
   }
 }
